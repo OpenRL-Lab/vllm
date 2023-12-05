a4ac70275003bd7a815db3ccecb152b36b4ed731
huangshiyu <huangsy1314@163.com>
2023-12-05 15:42:10 +0800

    update

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+TAG=yi_v2
+
+docker login harbor.4pd.io -u test -p Harbor@test01
 docker pull harbor.4pd.io/env/vllm/vllm-openai:latest
-docker build -t harbor.4pd.io/env/vllm/vllm-openai:yi_v2 -f vllm/Dockerfile_yi .
+docker build -t harbor.4pd.io/env/vllm/vllm-openai:${TAG} -f vllm/Dockerfile_yi .
+
+# push镜像
+docker tag harbor.4pd.io/env/vllm/vllm-openai:${TAG} harbor.4pd.io/lab-platform/env/vllm/vllm-openai:${TAG}
+docker push harbor.4pd.io/lab-platform/env/vllm/vllm-openai:${TAG}
